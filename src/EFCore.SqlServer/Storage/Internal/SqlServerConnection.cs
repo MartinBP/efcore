c46cf5b8e8e597d900041ee5f8946f6241071efb
Smit Patel <smitpatel@users.noreply.github.com>
2020-06-29 09:58:51 -0700

    Query: Auto-buffer data readers in SqlServer when split query & MARS off
    Resolves #21420

--- src/EFCore.SqlServer/Storage/Internal/SqlServerConnection.cs
+++ src/EFCore.SqlServer/Storage/Internal/SqlServerConnection.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Concurrent;
 using System.Data.Common;
 using JetBrains.Annotations;
 using Microsoft.Data.SqlClient;
@@ -27,6 +28,7 @@ namespace Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal
     {
         // Compensate for slow SQL Server database creation
         private const int DefaultMasterConnectionCommandTimeout = 60;
+        private static readonly ConcurrentDictionary<string, bool> _multipleActiveResultSetsEnabledMap;
 
         /// <summary>
         ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
@@ -86,6 +88,16 @@ namespace Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal
 
             return new SqlServerConnection(Dependencies.With(contextOptions));
         }
+
+        /// <summary>
+        ///     This is an internal API that supports the Entity Framework Core infrastructure and not subject to
+        ///     the same compatibility standards as public APIs. It may be changed or removed without notice in
+        ///     any release. You should only use it directly in your code with extreme caution and knowing that
+        ///     doing so can result in application failures when updating to a new Entity Framework Core release.
+        /// </summary>
+        public virtual bool IsMultipleActiveResultSetsEnabled
+            => _multipleActiveResultSetsEnabledMap.GetOrAdd(
+                ConnectionString, cs => new SqlConnectionStringBuilder(cs).MultipleActiveResultSets);
 
         /// <summary>
         ///     Indicates whether the store connection supports ambient transactions
